e0aca3877a1642ddd1304cca91fba5c189bba590
lakshithaa <lakshithaanatraj8@gmail.com>
2026-06-04 15:39:47 +0530

    javascript completed

---
+++ javascript/script.js
@@ -0,0 +1,102 @@
+// var a = 10;//reinitialize & redeclare
+// let b = 20;//reinitialize
+// const c = 30;//cannot reinitialize & redeclare
+
+// console.log(a);
+// console.log(b);
+// console.log(c); 
+
+// var num = 10;
+// var str = "Hello";
+// var undefine;
+// var Null=null;
+// var bool = true;
+
+// console.log( typeof num);
+// console.log( typeof str);
+// console.log( typeof undefine);
+// console.log( typeof Null);
+// console.log( typeof bool);        
+
+// var bigInt = 123n;
+// var symbol = Symbol("id");
+
+// console.log( typeof bigInt);
+// console.log( typeof symbol);
+
+
+// var arr=[10,20,30,40,];
+// console.log(typeof arr,arr);
+// var obj= {
+//     Name:"PRASATH",
+//     Dept:["EIE"]
+// }
+// console.log(typeof obj,obj);
+
+//arthimetic operator (+,-,*,%,/)
+
+// var a = 10;
+// var b = "laks";
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a%b);
+// console.log(a/b);
+
+//logical operator (&&,||,!)
+//                  &&    ||  
+// true true       true   true
+//  true false     false  true
+// false true      false  true
+// false false     false  false
+
+// var a = true;
+// var b = false;
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+//relational operator (>,>=,<,<=,==,!=)
+
+// a = 10;
+// b = "20";
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+
+//assignment operator(=,+=,-=,%=,/=,**=)
+// a=10;
+//b=20;
+//a=a+b;
+//a+=b;
+//console.log(a);
+//a-=b;
+//console.log(a);
+
+//unary operator(--,++)
+
+var a=10;
+
+console.log(a++);
+console.log(++a);
+
+var a=10;
+ 
+console.log(a--);
+console.log(--a);
+ 
+a=1;
+b=1;
+c=0;
+
+var result = a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+console.log(result);
+
+
+
+
