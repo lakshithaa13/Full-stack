70002fd4fefc0cea4d9ca4e3fa3127d2d6c69d8b
lakshithaa <lakshithaanatraj8@gmail.com>
2026-06-07 15:41:01 +0530

    useEffect and react-router-dom completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,24 +3,29 @@ import Home from './components/Home'
 import About from './components/About'
 import Contact from './components/Contact'
 import Service from './components/Service'
-import Navbar  from './Pages/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Navbar from "./Pages/Navbar"
 import Counter from './Pages/Counter'
+import Effects from './Pages/Effects'
 
-const App = ()  => {
+const App = () => {
   return (
-   //Fragment Tag
-   <>
-   <Navbar/>
-   <h1>Hello world</h1>
-   <h1>Hello world</h1>
-   <Parent/>
-   <Home/>
-   <About/>
-   <Contact/>
-   <Service/>
-   <Counter/>
-   </>
+    //Fragment Tag
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/service' element={<Service/>}></Route>
+        <Route path='/parent' element={<Parent/>}></Route>
+        <Route path='/usestate' element={<Counter/>}></Route>
+        <Route path='/useEffect' element={<Effects/>}></Route>
+        
+      </Routes>
+    </>
   )
+ 
 }
 
-export default App 
+          export default App 
